9dafedaaae2376e8680042129c99996602fe4a90
kzhang <kennycheung21@gmail.com>
2016-11-28 18:48:15 -0800

    Final commit for 1.6

--- sparkStreamingKMeans/src/main/scala/com/Kenny/streaming/SparkKMeans.scala
+++ sparkStreamingKMeans/src/main/scala/com/Kenny/streaming/SparkKMeans.scala
@@ -5,6 +5,8 @@ package com.Kenny.streaming
   */
 
 import java.io.{ByteArrayInputStream, DataInputStream}
+import java.util.Calendar
+import scopt.OptionParser
 
 import _root_.kafka.serializer.StringDecoder
 import kafka.serializer.StringDecoder
@@ -23,16 +25,16 @@ import org.apache.spark.streaming.StateSpec
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.Time
 import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.{sql, SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext, sql}
 import org.apache.spark.SparkContext._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka._
-import org.apache.spark.mllib.recommendation.Rating
-import org.apache.spark.scheduler.cluster.CoarseGrainedSchedulerBackend
+import org.apache.spark.mllib.clustering.StreamingKMeans
+import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.util.StatCounter
 import org.apache.spark.streaming.{State, StateSpec, Time}
-import org.apache.spark.mllib.regression.{LabeledPoint,StreamingLinearRegressionWithSGD}
+import org.apache.spark.mllib.regression.{LabeledPoint, StreamingLinearRegressionWithSGD}
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.streaming.State
@@ -74,11 +76,14 @@ object SparkKMeans {
 
     if (args.length < 2) {
       System.err.println(s"""
-                            |Usage: DirectKafkaWordCount <brokers> <topics>
+                            |Usage: SparkStreamingKmeans <brokers> <topics>
                             |  <brokers> is a list of one or more Kafka brokers
                             |  <topics> is a list of one or more kafka topics to consume from
+                            |  <group.id> is a string of consumer group identification
                             |  <batchInterval> is the batch interval which is also the sliding interval
-                            |  <windowLength> is duration of window
+                            |  <windowLength> is duration of window                            |
+                            |  <numClusters> is the number of Cluster
+                            |  <numDimensions> is the number of Dimensions
                             |  <securityProtocol> is the desired Kafka security protocol
         """.stripMargin)
       System.exit(1)
@@ -90,12 +95,12 @@ object SparkKMeans {
 
     //val admin = new HBaseAdmin(hbaseConf)
 
-    val Array(brokers, topics, batch, window, securityProtocol) =
-      if (args.length > 4) args else args :+ KafkaUtils.securityProtocolDefault
+    val Array(brokers, topics, groupId, batch, window, numClusters, numDimensions, securityProtocol) =
+      if (args.length > 7) args else args :+ KafkaUtils.securityProtocolDefault
     // Create context with 2 second batch interval
-    val sparkConf = new SparkConf().setAppName("KennyKafkaStream")
+    val sparkConf = new SparkConf().setAppName("KennyKafkaKMeans")
     val ssc = new StreamingContext(sparkConf, Seconds(batch.toLong))
-    ssc.checkpoint("/tmp/checkpoint")
+    ssc.checkpoint("/tmp/kMeansCheckpoint")
     val sc = ssc.sparkContext
 
     //val hc = new HiveContext(sc)
@@ -104,7 +109,7 @@ object SparkKMeans {
     // Create direct kafka stream with brokers and topics
     val topicsSet = topics.split(",").toSet
     val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers,
-      KafkaUtils.securityProtocolConfig -> securityProtocol, "auto.offset.reset" -> "smallest")
+      KafkaUtils.securityProtocolConfig -> securityProtocol, "auto.offset.reset" -> "smallest", "group.id" -> groupId)
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topicsSet)
 
@@ -115,23 +120,37 @@ object SparkKMeans {
     val keys = messages.map(_._1)//keys.print() it's null
     val lines = messages.map(_._2)
     //lines.print(1)
-    val records = lines.map(_.split(" ")).map((ws : Array[String]) => Record(ws(1).toDouble, ws(4).toLong))
-    //records.print(2)
+    val records = lines.map(_.split(" ")).map((ws : Array[String]) => "[" + ws(1) + "," + ws(4) + "]")
+    records.print(2)
     /*Record(99,1472004745)*/
 
-    val stateSpec = StateSpec.function(trackStateFunc _).numPartitions(2)
+    //val stateSpec = StateSpec.function(trackStateFunc _).numPartitions(2)
     //.window(Seconds(window.toLong),Seconds(batch.toLong))
-    val numStream = records.map(r => (r.RandomNum, 1))
+    val numStream = records.map(Vectors.parse)
+    numStream.print(2)
 
-    val numStateStream = numStream.mapWithState(stateSpec)
+    /*val numStateStream = numStream.mapWithState(stateSpec)
     numStateStream.print() //val tokenInfo = numStream.map(r => printTokenInfo(sparkConf))
 
     val NumStat = records.map(r => r
       .RandomNum).window(Seconds(window.toLong),Seconds(batch.toLong)).
       foreachRDD(rdd => { val s = rdd.stats; println("Statistics from last " +
-      ""+ window + " seconds: " + s)})
+      ""+ window + " seconds: " + s)})*/
 
+    val model = new StreamingKMeans().setK(numClusters.toInt).setDecayFactor(1.0).setRandomCenters(numDimensions.toInt, 0.0)
 
+    model.trainOn(numStream)
+
+    val predictions = model.predictOn(numStream)
+
+    predictions.foreachRDD { rdd =>
+      val modelString = model.latestModel().clusterCenters
+        .map(c => c.toString.slice(1, c.toString.length-1)).mkString("\n")
+      val predictString = rdd.map(p => p.toString).collect().mkString("\n")
+      val dateString = Calendar.getInstance().getTime.toString.replace(" ", "-").replace(":", "-")
+      println(dateString + ", -model: ", modelString)
+      println(dateString + ", -predictions: ", predictString)
+    }
 
     //val timeStat = records.window(Seconds(window.toLong),Seconds(batch.toLong)).map(r => r.time).foreachRDD(rdd => { val s = rdd.stats; println("From: " + s.min + " to: " + s.max)})
     /*reduce((r1:List[Double],r2:List[Double]) => { r1.zip(r2).flatten } )
